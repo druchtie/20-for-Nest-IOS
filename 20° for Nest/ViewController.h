c50030f87d6215d966ed4a9e7181e609c9cb9307
Danny Ruchtie <druchtie@wewantfur.com>
2015-05-14 10:19:57 +0200

    Initial Commit

---
+++ 20° for Nest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20° for Nest
+//
+//  Created by Danny Ruchtie on 5/14/15.
+//  Copyright (c) 2015 20°. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 20° for NestTests/_0__for_NestTests.m
@@ -0,0 +1,40 @@
+//
+//  _0__for_NestTests.m
+//  20° for NestTests
+//
+//  Created by Danny Ruchtie on 5/14/15.
+//  Copyright (c) 2015 20°. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface _0__for_NestTests : XCTestCase
+
+@end
+
+@implementation _0__for_NestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
